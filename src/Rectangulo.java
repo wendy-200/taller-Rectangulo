8ee2509f2e732dbfec0f62201ae8ecb0c4e76d90
wendy <Wortizm2@est.ups.edu.ec>
2024-06-16 17:13:10 -0500

    Update Rectangulo.java

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -1,2 +1,20 @@
 package PACKAGE_NAME;public class Rectangulo {
 }
+public static void main(String[] args) {
+        System.out.println("Rectángulo  alto 7  ancho 7:");
+        printRectangle(7, 7);
+        System.out.println("\nRectángulo alto 5  ancho 14:");
+        printRectangle(5, 14);
+        System.out.println("\nRectángulo alto 8  ancho 20:");
+        printRectangle(8, 20);
+    }
+
+    public static void printRectangle(int height, int width) {
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= width; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
